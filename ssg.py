ba8b7b4a9c34a38263360a4c7028173cc2e1ae88
Steven Yu <Stevenyu@mail.weber.edu>
2020-10-04 23:39:42 -0600

    Completed module

--- ssg.py
+++ ssg.py
@@ -1,10 +1,18 @@
 import typer
+import ssg.parsers
 from ssg.site import Site
 
-
 def main(source="content", dest="dist"):
-    config = {"source": source, "dest": dest}
-    Site(**config).build()
+    config = {
+        "source": source,
+        "dest": dest,
+        "parsers": [
+            ssg.parsers.MarkdownParser(),
+            ssg.parsers.ReStructuredTextParser(),
+            ssg.parsers.ResourceParser(),
+        ],
+    }
 
+    Site(**config).build()
 
 typer.run(main)
--- ssg/site.py
+++ ssg/site.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-
 class Site:
     def __init__(self, source, dest):
         self.source = Path(source)
@@ -9,7 +8,18 @@ class Site:
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
+        
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
 
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            print("Not Implemented")
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
